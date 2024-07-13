8c8e85075fb2888503fe4c33df590c20a3bd9b53
ISA531 <isaacahmed@icloud.com>
2024-07-13 14:38:10 -0500

    and here's the jumper
    jumping man

--- LilGuyShooter/Scripts/jumper.gd
+++ LilGuyShooter/Scripts/jumper.gd
@@ -1,12 +1,13 @@
 extends RigidBody2D
 
-
+@onready var my_timer = $Timer
 const SPEED = 100.0
 const JUMP_VELOCITY = -50.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var time=0;
 var on_ground = 0
 
 func _on_Area2D_body_entered(body: Node) -> void:
@@ -17,7 +18,9 @@ func _on_Area2D_body_exited(body: Node) -> void:
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	my_timer.timeout.connect(_on_timer_timeout)
+	my_timer.wait_time = 3.0
+	my_timer.start()
 
 #func _ready():
 #	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
@@ -26,14 +29,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var rng = RandomNumberGenerator.new()
-	var waitTime = rng.randf_range(0.3, 2.5)
-	var direction = rng.randi_range(1, 3)
-	# 2/3 will go left 1/3 go right
-	await get_tree().create_timer(waitTime).timeout
-	if on_ground>0:
-		if direction>1:
-			apply_central_impulse(Vector2(-300, -300))
-		else:
-			apply_central_impulse(Vector2(300, -300))
+	pass
 		
+		
+func _jump(rng):
+	#var rng = RandomNumberGenerator.new()
+	#var waitTime = rng.randf_range(0.3, 2.5)
+	var direction = rng.randi_range(1, 3)
+	#if on_ground>0:
+	if direction>1:
+		apply_central_impulse(Vector2(-300, -300))
+		print("left gaming")
+	else:
+		apply_central_impulse(Vector2(300, -300))
+		print("right gaming")
+
+func _on_timer_timeout():
+	print("Timer has timed out!")
+	my_timer.stop()
+	var rng = RandomNumberGenerator.new()
+	_jump(rng)
+	var waitTime = rng.randf_range(0.3, 2.5)
+	my_timer.wait_time =waitTime
+	my_timer.start()
