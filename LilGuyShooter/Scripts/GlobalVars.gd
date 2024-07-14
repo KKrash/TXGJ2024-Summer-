aa5968d7992aef82a543457ae1e8923b7918cd52
KKrash <112344026+KKrash@users.noreply.github.com>
2024-07-14 15:13:07 -0500

    health (kind of integrated) and fuel bar (fully integrated)

--- LilGuyShooter/Scripts/GlobalVars.gd
+++ LilGuyShooter/Scripts/GlobalVars.gd
@@ -1,2 +1,7 @@
 extends Node
 var score
+var max_lives = 3
+var lives = max_lives
+var fuelPercentage = 100
+var currentFuel = fuelPercentage/4.0
+var fuelPercentageSpeed = 1
--- LilGuyShooter/Scripts/Starting Menu.gd
+++ LilGuyShooter/Scripts/Starting Menu.gd
@@ -1,14 +1,4 @@
 extends Control
-#var root = get_tree().get_root()
-#
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass # Replace with function body.
-#
-#func _on_button_pressed():
-	#var level = get_tree().get_current_scene()
-	#root.remove_child(level)
-	#level.call_deferred("free")
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://combined_resource_+_score.tscn")
