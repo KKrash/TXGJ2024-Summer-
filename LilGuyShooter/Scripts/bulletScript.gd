5532207e52e4d53bae9b1224e7d31f1df955ba48
ISA531 <isaacahmed@icloud.com>
2024-07-14 14:11:16 -0500

    bullets

--- LilGuyShooter/Scripts/bulletScript.gd
+++ LilGuyShooter/Scripts/bulletScript.gd
@@ -1,15 +1,16 @@
 extends RigidBody2D
 
-var callable = Callable(self, "_on_BodyEntered")
+#var callable = Callable(self, "_on_BodyEntered")
 #var explosionScene : PackedScene = preload("res://Assets/Misc/Projectiles/ExplosionEnemy.tscn")
 @onready var sprite = $AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#connect("body_entered", self.get_node("Area2D"), "_on_Area_body_entered")
 	apply_central_impulse(Vector2(-300, -300))
-	connect("body_entered",callable)
+	#connect("body_entered",callable)
 
-func on_Area2D_body_entered(body):
+func body_entered (body):
+	print("BULLETHIT")
 	if body.has_method("hit"):
 		body.hit()
 	queue_free() 
--- LilGuyShooter/Scripts/bulletcollision.gd
+++ LilGuyShooter/Scripts/bulletcollision.gd
@@ -3,16 +3,8 @@ var explosionScene : PackedScene = preload("res://Assets/Misc/Projectiles/Explos
 func _physics_process(delta):
 	pass	
 
-func _on_body_entered(body):
-	var x = get_parent()
-	var p = x.position
-	print(p)
-	if explosionScene != null:
-		var explosion = explosionScene.instantiate()
-		if explosion != null:
-			# Set the position of the explosion
-			explosion.position = p
-
-			# Add the explosion to the scene
-			get_tree().root.add_child(explosion)
-	x.queue_free()
+func body_entered (body):
+	print("BULLETHIT")
+	if body.has_method("hit"):
+		body.hit()
+	queue_free() 
